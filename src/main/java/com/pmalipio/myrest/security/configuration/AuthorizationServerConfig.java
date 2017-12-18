5d08324c64c15123c448f99af1020cdb5cddf8c2
Pedro Alipio <pedro.alipio@gmail.com>
2017-12-18 23:00:46 +0000

    Added copyright notice in source files

--- src/main/java/com/pmalipio/myrest/security/configuration/AuthorizationServerConfig.java
+++ src/main/java/com/pmalipio/myrest/security/configuration/AuthorizationServerConfig.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2017.  Pedro Alípio, All Rights Reserved.
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee. Permission to modify the code and to distribute modified
+ * code is also granted without any restrictions.
+ */
 package com.pmalipio.myrest.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,7 +15,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +23,6 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
--- src/main/java/com/pmalipio/myrest/security/models/User.java
+++ src/main/java/com/pmalipio/myrest/security/models/User.java
@@ -1,11 +1,18 @@
+/*
+ * Copyright (c) 2017.  Pedro Alípio, All Rights Reserved.
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee. Permission to modify the code and to distribute modified
+ * code is also granted without any restrictions.
+ */
 package com.pmalipio.myrest.security.models;
 
 import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import java.util.ArrayList;
